34355a47c6381fc9f66637b4cc12eb830d631e6c
Marcus Wiseman <marcus.e.wiseman@gmail.com>
2019-04-12 03:33:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,3 +9,23 @@ function subtract(a,b) {
 function multiply(a,b) {
   return a * b;
 }
+
+function divide(a,b) {
+  return a / b;
+}
+
+function inc(n) {
+  return n += n;
+}
+
+function dec(n) {
+  return n -= n;
+}
+
+function makeInt(n) {
+  return parseInt("n", 10);
+}
+
+function preserveDecimal(n) {
+  return parseInt("n", 10);
+}
